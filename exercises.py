e2c3b366266ab090a20828edef65188a1de9b539
najbrian <najbrian@gmail.com>
2024-09-05 16:12:03 -0700

    exercise 6

--- exercises.py
+++ exercises.py
@@ -94,4 +94,24 @@ def convert_temperature(temp, unit):
 def sum_to(n) :
   return sum(range(1, n + 1))
 
-print('Exercise 5:', sum_to(10))
+# print('Exercise 5:', sum_to(10))
+
+
+
+
+# Exercise 6: Find the Largest Number
+#
+# Write a function named `largest` that takes three integers as arguments and returns the largest of them.
+#
+# Examples:
+# largest(1, 2, 3) should return 3.
+# largest(10, 4, 2) should return 10.
+#
+# Define your function and test it with different inputs.
+
+def largest(*args):
+  return max(args)
+  
+
+# print('Exercise 6:', largest(1, 2, 3))
+# print('Exercise 6:', largest(10, 4, 2))

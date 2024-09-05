76255f0a31b7393e41b7649a38a7d0136061b36b
najbrian <najbrian@gmail.com>
2024-09-05 15:44:56 -0700

    exercise 3

--- exercises.py
+++ exercises.py
@@ -32,5 +32,25 @@ def calculate_area_triangle (b, h):
 def simple_interest(p, r, y):
   return (p * (r/100) * y)
 
-print('Exercise 2:', simple_interest(1000, 5, 2))
-print('Exercise 2:', simple_interest(1500, 3.5, 5))
+# print('Exercise 2:', simple_interest(1000, 5, 2))
+# print('Exercise 2:', simple_interest(1500, 3.5, 5))
+
+
+
+# Exercise 3: Apply a Discount
+#
+# Write a function named `apply_discount` that takes a product's price and a discount percentage (from 0 to 100).
+# Return the new price after applying the discount.
+#
+# Examples:
+# apply_discount(100, 25) should return 75.
+# apply_discount(80, 10) should return 72.
+#
+# Define your function and call it to display the discounted price.
+
+def apply_discount(price, discount):
+  return int(price * (1 - (discount/100)))
+
+
+# print('Exercise 3:', apply_discount(100, 25))
+# print('Exercise 3:', apply_discount(80, 10))

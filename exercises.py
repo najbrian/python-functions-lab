fd96c9cc6becde0bd719392faaa9b8e2736d8220
najbrian <najbrian@gmail.com>
2024-09-05 16:22:19 -0700

    exercise 7

--- exercises.py
+++ exercises.py
@@ -115,3 +115,20 @@ def largest(*args):
 
 # print('Exercise 6:', largest(1, 2, 3))
 # print('Exercise 6:', largest(10, 4, 2))
+
+
+
+
+# Exercise 7: Calculate a Tip
+#
+# Create a function called `calculate_tip`. It should take the bill amount and the tip percentage (as a whole number).
+# The function should return the amount of the tip.
+#
+# Examples:
+# calculate_tip(50, 20) should return 10.
+#
+# Write your function and test its output below.
+def calculate_tip(bill, tip):
+  return bill * (tip/100)
+
+print('Exercise 7:', calculate_tip(50, 23))
